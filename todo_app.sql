645cd4b6f8968666de84e50153e7005b98a577ee
Ian Bovard <thebestpantsintown@gmail.com>
2017-07-22 15:22:50 -1000

    initial table set

--- todo_app.sql
+++ todo_app.sql
@@ -4,3 +4,9 @@ DROP USER IF EXISTS michael;
 CREATE USER michael WITH ENCRYPTED PASSWORD 'stonebreaker';
 
 CREATE DATABASE todo_app WITH OWNER michael;
+
+\c todo_app;
+
+CREATE TABLE tasks ( id SERIAL NOT NULL, title VARCHAR(255) NOT NULL, description TEXT, created_at TIMESTAMP WITHOUT time zone NOT NULL DEFAULT NOW(), updated_at TIMESTAMP WITHOUT time zone, completed BOOL NOT NULL DEFAULT false);
+
+\d tasks;
