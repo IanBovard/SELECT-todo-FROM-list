2f1b49d99bcb2b401b04aa04d0fe0f8ab275c30c
Ian Bovard <thebestpantsintown@gmail.com>
2017-07-22 16:27:09 -1000

    select not yet completed

--- todo_app.sql
+++ todo_app.sql
@@ -25,4 +25,4 @@ SELECT title FROM tasks WHERE completed_at IS NULL;
 
 UPDATE tasks SET completed_at = NOW() WHERE title = 'Study SQL';
 
-SELECT * FROM tasks;
+SELECT title, description FROM tasks WHERE completed_at IS NULL;
