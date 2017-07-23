3c3a11bfb4699083bfc9b509acd17a7708cfc3a2
Ian Bovard <thebestpantsintown@gmail.com>
2017-07-22 16:08:10 -1000

    new task

--- todo_app.sql
+++ todo_app.sql
@@ -9,11 +9,14 @@ CREATE DATABASE todo_app WITH OWNER michael;
 
 \c todo_app;
 
-CREATE TABLE tasks ( id SERIAL NOT NULL, title VARCHAR(255) NOT NULL, description TEXT, created_at TIMESTAMP WITHOUT time zone NOT NULL DEFAULT NOW(), updated_at TIMESTAMP WITHOUT time zone, completed BOOL NOT NULL DEFAULT false);
+CREATE TABLE tasks ( id SERIAL NOT NULL, title VARCHAR(255) NOT NULL, description TEXT, created_at TIMESTAMP WITHOUT time zone NOT NULL DEFAULT NOW (), updated_at TIMESTAMP WITHOUT time zone, completed BOOL NOT NULL DEFAULT false );
 
 ALTER TABLE tasks DROP COLUMN completed;
 ALTER TABLE tasks ADD COLUMN completed_at TIMESTAMP WITHOUT time zone DEFAULT NULL;
 ALTER TABLE tasks ALTER COLUMN updated_at NOT NULL;
-ALTER TABLE tasks ALTER COLUMN updated_at SET DEFAULT NOW();
+ALTER TABLE tasks ALTER COLUMN updated_at SET DEFAULT NOW ();
 
-\d tasks;
+INSERT INTO tasks VALUES ( DEFAULT, 'Study SQL', 'Complete this exercise', NOW (), NOW(), NULL );
+
+
+SELECT * FROM tasks;
